8effa001b7c3399d05610e8faefbb147de3d49bf
Stanislav <stanislav@ventureslab.io>
2025-02-08 19:27:10 +0200

    connection check

--- .mocharc.js
+++ .mocharc.js
@@ -1,4 +1,4 @@
-// .mocharc.js - Mocha config file some change
+// .mocharc.js - some change to check connection
 const {colors, symbols} = 
   require('mocha/lib/reporters/base');
 // Change the default console UI colors
